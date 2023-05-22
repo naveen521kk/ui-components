4f942f3c07bdaefbc0b825bdf7a5478dcab8a1c8
Naveen M K <naveen521kk@gmail.com>
2023-05-22 10:54:40 +0530

    Add autoprefixer to postcss config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import autoprefixer from "autoprefixer";
 import postcss from "rollup-plugin-postcss";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -29,7 +30,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
+      postcss({ plugins: [autoprefixer()], modules: true, use: ["sass"] }),
       terser(),
     ],
   },
