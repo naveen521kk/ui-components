5fc35e05242af84bf5c6a16dfb9fa4d7b3a47b46
Naveen M K <naveen521kk@gmail.com>
2023-05-22 11:41:56 +0530

    Start using styled components

--- src/components/scroll-progress-bar/scroll-progress-bar.tsx
+++ src/components/scroll-progress-bar/scroll-progress-bar.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import {createPortal} from "react-dom";
-import {useScroll, animated} from "@react-spring/web";
-import * as S from "./scroll-progress-bar.module.scss";
+import { createPortal } from "react-dom";
+import { useScroll, animated } from "@react-spring/web";
+import styled from "styled-components";
+
+const StyledDiv = styled(animated.div)`
+  height: 4px;
+  left: 0px;
+  position: fixed;
+  top: 0px;
+  z-index: 9999;
+  background: var(--nv-body-color, #fff);
+`;
 
 const ScrollProgressBar = () => {
-    const {scrollYProgress} = useScroll();
+  const { scrollYProgress } = useScroll();
 
-    return (
-        <>
-            {createPortal(
-                <animated.div
-                    style={{
-                        width: scrollYProgress.to({
-                            range: [0, 1],
-                            output: ["0%", "100%"]
-                        })
-                    }}
-                    className={S.scrollProgressBar}
-                />,
-                document.body
-            )}
-        </>
-    );
+  return (
+    <>
+      {createPortal(
+        <StyledDiv
+          style={{
+            width: scrollYProgress.to({
+              range: [0, 1],
+              output: ["0%", "100%"],
+            }),
+          }}
+        />,
+        document.body
+      )}
+    </>
+  );
 };
 
 export default ScrollProgressBar;
